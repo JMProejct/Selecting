664d3551f9b2d767a1b080dfb43c123d46a7b582
adward27 <whalsrnr2741@gmail.com>
2025-03-27 23:07:44 +0900

    <feat>: 로그인 할때 아이디로 사용할 username 컬럼 추가

--- src/main/java/selecting/platform/model/User.java
+++ src/main/java/selecting/platform/model/User.java
@@ -23,6 +23,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
+    private String username;
     private String email;
     private String name;
     private String password;
