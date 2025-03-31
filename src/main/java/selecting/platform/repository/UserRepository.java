898449dbb87681a43d978ed2d53ad836d31ba213
adward27 <whalsrnr2741@gmail.com>
2025-03-31 15:07:05 +0900

    <feat> 메소드 추가

--- src/main/java/selecting/platform/repository/UserRepository.java
+++ src/main/java/selecting/platform/repository/UserRepository.java
@@ -3,6 +3,12 @@ package selecting.platform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import selecting.platform.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String email);
+
+    boolean existsByEmail(String email);
 }
