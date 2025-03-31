e61ee5d7e96e7dd904167b7733c4a661051ca89a
adward27 <whalsrnr2741@gmail.com>
2025-03-31 15:10:06 +0900

    <feat> OAuth2 유저 관련 DetailService 추가

--- src/main/java/selecting/platform/service/CustomOAuth2UserService.java
+++ src/main/java/selecting/platform/service/CustomOAuth2UserService.java
@@ -40,7 +40,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
         String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
-        User existData = userRepository.findByUsername(username);
+        User existData = userRepository.findByUsername(username).orElse(null);
 
         // 신규 회원인 경우
         if(existData == null) {
