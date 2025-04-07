ad436ba0f5e6ecf6061d10ead5e165e9a987a265
adward27 <whalsrnr2741@gmail.com>
2025-04-07 16:42:01 +0900

    <refactor> UserService 에 있던 유저 정보 가져오는 메소드 AuthUtil 로 옮김

--- src/main/java/selecting/platform/service/UserService.java
+++ src/main/java/selecting/platform/service/UserService.java
@@ -36,4 +36,26 @@ public class UserService {
         return userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElse(null); // 없으면 null 반환
     }
+
+    public User getUserProfile(String token) {
+        if(token == null) {
+            throw new IllegalArgumentException(ErrorCode.AUTH_MISSING_TOKEN.getMessage());
+        }
+        if(jwtUtil.isExpired(token)) {
+            throw new IllegalArgumentException(ErrorCode.AUTH_EXPIRED_TOKEN.getMessage());
+        }
+        String username = jwtUtil.getUsername(token);
+        CustomUserDetails userDetails = (CustomUserDetails) customUserDetailsService.loadUserByUsername(username);
+        return userDetails.getUser();
+    }
+
+    public String getUserRole(String token) {
+        if(token == null) {
+            throw new IllegalArgumentException(ErrorCode.AUTH_MISSING_TOKEN.getMessage());
+        }
+        if(jwtUtil.isExpired(token)) {
+            throw new IllegalArgumentException(ErrorCode.AUTH_EXPIRED_TOKEN.getMessage());
+        }
+        return jwtUtil.getRole(token);
+    }
 }
