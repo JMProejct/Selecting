39b6758ac4815c9f6606173fa6cfabbfe3f13f85
adward27 <whalsrnr2741@gmail.com>
2025-04-16 19:58:33 +0900

    <feat> sentAt 추가 및 기본값 설정

--- src/main/java/selecting/platform/dto/chatMessage/ChatMessageDto.java
+++ src/main/java/selecting/platform/dto/chatMessage/ChatMessageDto.java
@@ -3,11 +3,14 @@ package selecting.platform.dto.chatMessage;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Getter
 @Setter
 public class ChatMessageDto {
     private Integer roomId;
     private Integer senderId;
     private Integer receiverId;
-    private String message;
+    private String messageText;
+    private Timestamp sentAt = new Timestamp(System.currentTimeMillis());
 }
