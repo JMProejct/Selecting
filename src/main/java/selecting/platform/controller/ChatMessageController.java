2dd1494b8cdad5d1f64404d529faee5f35e89c05
adward27 <whalsrnr2741@gmail.com>
2025-04-16 19:59:13 +0900

    <feat> 메시지 라우팅

--- src/main/java/selecting/platform/controller/ChatMessageController.java
+++ src/main/java/selecting/platform/controller/ChatMessageController.java
@@ -5,12 +5,16 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import selecting.platform.dto.chatMessage.ChatMessageDto;
+import selecting.platform.service.chatMessage.ChatMessageProducerService;
 
 @Controller
 @RequiredArgsConstructor
 public class ChatMessageController {
 
+    private final ChatMessageProducerService producer;
+
     @MessageMapping("/chat/send")
     public void sendMessage(@Payload ChatMessageDto chatMessageDto) {
+        producer.send(chatMessageDto);
     }
 }
