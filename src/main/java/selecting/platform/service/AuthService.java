6ca0dc03ed9c6f0078a5efe748d4a9183f8d53d4
adward27 <whalsrnr2741@gmail.com>
2025-04-23 12:23:36 +0900

    <refactor> 에러코드 사용 및 @RequiredArgsConstructor 사용

--- src/main/java/selecting/platform/service/AuthService.java
+++ src/main/java/selecting/platform/service/AuthService.java
@@ -2,6 +2,7 @@ package selecting.platform.service;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,16 +11,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import selecting.platform.error.ErrorCode;
+import selecting.platform.error.exception.CustomException;
 import selecting.platform.jwt.JWTUtil;
 import selecting.platform.model.Enum.ProviderType;
 import selecting.platform.model.Enum.Role;
 import selecting.platform.model.User;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
@@ -27,21 +30,12 @@ public class AuthService {
     private final UserService userService;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public AuthService(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UserService userService, BCryptPasswordEncoder passwordEncoder) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
     public void login(String usernameOrEmail, String password, HttpServletResponse response) throws IOException {
         try {
             User user = userService.findByUsernameOrEmail(usernameOrEmail);
 
             if (user == null) {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                response.getWriter().write("존재하지 않는 사용자입니다.");
-                return;
+                throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
             }
 
             String username = user.getUsername();
@@ -62,11 +56,11 @@ public class AuthService {
 
     public ResponseEntity<?> registerUser(User user) {
         if (userService.existsByUsername(user.getUsername())) {
-            return ResponseEntity.badRequest().body("이미 존재하는 아이디입니다.");
+            throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
         }
 
         if (userService.existsByEmail(user.getEmail())) {
-            return ResponseEntity.badRequest().body("이미 존재하는 이메일입니다.");
+            throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
         }
 
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
