ba500e846d1721e8c3d40c0fe20c585c264034d5
adward27 <whalsrnr2741@gmail.com>
2025-04-23 12:24:55 +0900

    <feat> Chat 관련 DB 메소드 추가

--- src/main/java/selecting/platform/repository/ChatMessageRepository.java
+++ src/main/java/selecting/platform/repository/ChatMessageRepository.java
@@ -1,7 +1,16 @@
 package selecting.platform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import selecting.platform.model.ChatMessage;
+import selecting.platform.model.ChatRoom;
+
+import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    Optional<ChatMessage> findTopByRoomOrderBySentAtDesc(ChatRoom chatRoom);
+
+    @Query("SELECT COUNT(m) FROM ChatMessage m WHERE m.room = :room AND m.sender.userId <> :userId AND m.messageRead = 'Unread'")
+    int countUnreadMessagesByRoomAndNotSender(@Param("room") ChatRoom room, @Param("userId") Integer userId);
 }
--- src/main/java/selecting/platform/repository/ChatRoomRepository.java
+++ src/main/java/selecting/platform/repository/ChatRoomRepository.java
@@ -3,5 +3,8 @@ package selecting.platform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import selecting.platform.model.ChatRoom;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
+    public List<ChatRoom> findByUserUserId(Integer userId);
 }
