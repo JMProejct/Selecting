0daef5285d3e30328e7a6a5f09d8815773a9c1bb
adward27 <whalsrnr2741@gmail.com>
2025-04-23 12:22:54 +0900

    <feat> 채팅방 생성 및 조회 기능 추가

---
+++ src/main/java/selecting/platform/controller/ChatRoomController.java
@@ -0,0 +1,33 @@
+package selecting.platform.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import selecting.platform.dto.chatRoom.ChatRoomDto;
+import selecting.platform.dto.chatRoom.ChatRoomResponseDto;
+import selecting.platform.security.CustomUserDetails;
+import selecting.platform.service.ChatRoomService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/chatRoom")
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Integer> createChatRoom(@RequestBody ChatRoomDto chatRoomDto,
+                                                  @AuthenticationPrincipal CustomUserDetails userDetails) {
+        chatRoomService.createChatRoom(chatRoomDto, userDetails.getUserId());
+        return ResponseEntity.ok(chatRoomDto.getRoomId());
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<List<ChatRoomResponseDto>> getMyChatRooms
+            (@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(chatRoomService.getChatRooms(userDetails.getUserId()));
+    }
+}
---
+++ src/main/java/selecting/platform/service/ChatRoomService.java
@@ -0,0 +1,78 @@
+package selecting.platform.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import selecting.platform.dto.chatRoom.ChatRoomDto;
+import selecting.platform.dto.chatRoom.ChatRoomResponseDto;
+import selecting.platform.error.ErrorCode;
+import selecting.platform.error.exception.CustomException;
+import selecting.platform.model.ChatMessage;
+import selecting.platform.model.ChatRoom;
+import selecting.platform.model.ServicePost;
+import selecting.platform.model.User;
+import selecting.platform.repository.ChatMessageRepository;
+import selecting.platform.repository.ChatRoomRepository;
+import selecting.platform.repository.ServicePostRepository;
+import selecting.platform.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
+    private final ServicePostRepository servicePostRepository;
+    private final ChatMessageRepository chatMessageRepository;
+
+    // 채팅방 생성
+    public void createChatRoom(ChatRoomDto chatRoomDto,Integer userId) {
+
+        chatRoomDto.setUserId(userId);
+
+        ServicePost post = servicePostRepository.findById(chatRoomDto.getPostId())
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+
+        User user = userRepository.findById(chatRoomDto.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        ChatRoom chatRoom = ChatRoom.builder()
+                .post(post)
+                .user(user)
+                .createdAt(chatRoomDto.getCreatedAt())
+                .build();
+
+        chatRoomRepository.save(chatRoom);
+    }
+
+    // 채팅방 리스트 형식으로 조회
+    public List<ChatRoomResponseDto> getChatRooms(Integer userId) {
+        List<ChatRoom> chatRooms = chatRoomRepository.findByUserUserId(userId);
+
+        return chatRooms.stream().map(chatRoom -> {
+            // 상대방 가져오기
+            User chatPartner = chatRoom.getUser().getUserId().equals(userId)
+                    ? chatRoom.getPost().getUser()
+                    : chatRoom.getUser();
+
+            // 마지막 메시지 가져오기
+            ChatMessage lastMessage = chatMessageRepository
+                    .findTopByRoomOrderBySentAtDesc(chatRoom)
+                    .orElse(null);
+
+            int unreadCount = chatMessageRepository
+                    .countUnreadMessagesByRoomAndNotSender(chatRoom, userId);
+
+            return ChatRoomResponseDto.builder()
+                    .roomId(chatRoom.getRoomId())
+                    .chatPartnerId(chatPartner.getUserId())
+                    .chatPartnerName(chatPartner.getName())
+                    .chatPartnerProfileImage(chatPartner.getProfileImage())
+                    .lastMessage(lastMessage != null ? lastMessage.getMessageText() : "")
+                    .lastMessageTime(lastMessage != null ? lastMessage.getSentAt().toString() : null)
+                    .unreadMessageCount(unreadCount)
+                    .build();
+        }).toList();
+    }
+}
